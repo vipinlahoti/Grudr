d04cecd44acad1789ce18c23066d4eb229d956b1
vipin.lahoti <vipin.lahoti@ASLEEPSTUFFEND.workdayinternal.com>
2020-03-31 22:02:07 +0530

    header icon fix

--- packages/core/lib/modules/icons.js
+++ packages/core/lib/modules/icons.js
@@ -6,42 +6,17 @@ import Grudr from './config.js';
  * @param {string} [iconClass] - an optional class to assign to the icon
  */
 Grudr.utils.getIcon = function (iconName, iconClass) {
-  var icons = Grudr.utils.icons;
-  var iconCode = !!icons[iconName] ? icons[iconName] : iconName;
-  var iconClass = (typeof iconClass === 'string') ? ' '+iconClass : '';
-  return '<i class="icon fa fa-fw fa-' + iconCode + ' icon-' + iconName + iconClass+ '" aria-hidden="true"></i>';
+  const icons = Grudr.utils.icons;
+  const iconCode = !!icons[iconName] ? icons[iconName] : iconName;
+  iconClass = (typeof iconClass === 'string') ? ' ' + iconClass : '';
+  const c = 'material-icons icon' + ' icon-' + iconName + iconClass;
+  return '<i class="' + c + '" aria-hidden="true">' + iconCode + '</i>';
 };
 
 /**
  * A directory of icon keys and icon codes
  */
 Grudr.utils.icons = {
-  expand: "angle-right",
-  collapse: "angle-down",
-  next: "angle-right",
-  close: "times",
-  upvote: "chevron-up",
-  voted: "check",
-  downvote: "chevron-down",
-  facebook: "facebook-square",
-  twitter: "twitter",
-  googleplus: "google-plus",
-  linkedin: "linkedin-square",
-  comment: "comment-o",
-  share: "share-square-o",
-  more: "ellipsis-h",
-  menu: "bars",
-  subscribe: "envelope-o",
-  delete: "trash-o",
-  edit: "pencil",
-  popularity: "fire",
-  time: "clock-o",
-  best: "star",
-  search: "search",
-  edit: "pencil",
-  approve: "check-circle-o",
-  reject: "times-circle-o",
-  views: "eye",
-  clicks: "mouse-pointer", 
-  score: "line-chart"
+  account: 'account_circle',
+  person_add: 'person_add'
 };
